61b7a6889cbd00a68d7fb860d371ffb5b4b555aa
Andrea Sanna <47662221+AthlonsDev@users.noreply.github.com>
2025-11-30 16:25:37 +0000

    fix: update CardFile component to set default model type and improve UI elements; enhance model_1 with tutorial and usage instructions

--- src/pages/model_1.jsx
+++ src/pages/model_1.jsx
@@ -21,6 +21,7 @@ export default function Model_1(){
         <>
         {/* Sidebar Menu */}
         <Sidebar/>
+        <p>v.1.1</p>
             <div class="text-center">
                 <h1>Speech Recognition</h1>
             </div>
@@ -30,12 +31,41 @@ export default function Model_1(){
             <CardFile text={data} setTranscription={handleSetTranscription}/>
 
             <h2 class="text-center mt-4 mb-4">
-                Transcription
-                {/* {getFiles()} */}
+                Quick Tutorial
+                
             </h2>
 
+            <h3>
+                How Does It Work?
+            </h3>
             <p>
-                {transcription ? <div dangerouslySetInnerHTML={{ __html: transcription }} /> : "No Transcription Available"}
+                Our Speech Recognition model utilizes advanced machine learning algorithms to convert spoken language into written text. By analyzing audio input, the model identifies phonetic patterns and translates them into accurate transcriptions.
+            </p>
+
+            <h3>
+                Step-by-Step Guide
+            </h3>
+            <ol>
+                <li><strong>Upload Your Audio File:</strong> Click on the upload button to select your audio file from your device.</li>
+                <li><strong>Select Model Type:</strong> Choose the appropriate model type that best suits your audio characteristics.</li>
+                <li><strong>Initiate Transcription:</strong> Click the 'Upload' button to send your file for processing. The model will analyze the audio and generate a transcription.</li>
+                <li><strong>Cloud Processing: </strong> The audio file is processed in the cloud and saved in the database, so the app can be accessed from any device.</li>
+                <li><strong>View Results:</strong> Once processing is complete, the transcribed text will be available to download as a Word document.</li>
+            </ol>
+
+            <h3>
+                Type of Models
+            </h3>
+            <ul>
+                <li><strong>Notes Transcription:</strong> Suitable for single speaker audio.</li>
+                <li><strong>Meeting Transcription:</strong> Designed for multi-speaker audio, such as business meetings or conferences.</li>
+            </ul>
+            <h3>
+                Disclaimer
+            </h3>
+            <p>
+                This model is running in a testing environment, so the processing of the audio files is still quite slow.
+                When a more advaced environment will be available, the speed and accuracy of the transcriptions will improve significantly.
             </p>
         </>
     );
