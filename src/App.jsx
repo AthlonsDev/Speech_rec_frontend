fc586a684d8091dfacd7133f977ac1d065a02210
athlonsdev <athlons91@gmail.com>
2026-02-04 15:29:44 +0000

    removed model_2 route fixing splashscreen

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Model_1 from './pages/model_1.jsx';
-import Model_2 from './pages/model_2.jsx';
 import Splash from './pages/Splash.jsx';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
      <Routes>
       <Route path="/" element={<Model_1 />}></Route>
       <Route path='/model_1' element={<Model_1 />}></Route>
-      <Route path='/model_2' element={<Model_2 />}></Route>
      </Routes>
     </>
   );
